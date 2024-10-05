030a9411661a6288091b1004dad1ddce829c8194
prash <prashant.nagoorkar@gmail.com>
2024-10-05 13:50:29 -0400

    Moved EmbeddedJsonSourcesAttribute down to Baubit

--- Baubit.xUnit.Test/Fixture/TestBroker.cs
+++ Baubit.xUnit.Test/Fixture/TestBroker.cs
@@ -1,4 +1,6 @@
-﻿namespace Baubit.xUnit.Test.Fixture
+﻿using Baubit.Configuration;
+
+namespace Baubit.xUnit.Test.Fixture
 {
     [EmbeddedJsonSources("Baubit.xUnit.Test;Fixture.testBroker.json")]
     public class TestBroker : ITestBroker
